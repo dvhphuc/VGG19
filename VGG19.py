614b8005e117b133b0bbc0631df858a448c6feee
phuc <75090404+dvhphuc@users.noreply.github.com>
2022-03-14 18:29:21 +0700

    Add FC_Layers to VGG19.py

--- VGG19.py
+++ VGG19.py
@@ -7,7 +7,7 @@ import torchvision
 class VGG19(nn.Module):
     def __init__(self,num_classes,in_channel):
         super(VGG19,self).__init__()
-        self.CNN_Layers = nn.Sequential(
+        self.Conv_layers = nn.Sequential(
             nn.Conv2d(in_channel,64,kernel_size=3,padding=1),
             nn.Relu(inplace=False),
             nn.MaxPool1d(kernel_size=2,stride=2),
@@ -29,13 +29,62 @@ class VGG19(nn.Module):
             nn.MaxPool1d(kernel_size=2,stride=2),
         )
         self.FC_Layers = nn.Sequential(
-
+            nn.Linear(25088,4096),
+            nn.Relu(),
+            nn.Dropout2d(0.5),
+            nn.Linear(4096,4096),
+            nn.Relu(),
+            nn.Dropout2d(0.5),          
+            nn.Liner(4096,out_features = self.num_classes)  
         )
     
     def forward(self,x):
-        
+        x = self.Conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.FC_Layers(x)
         return x
-    
+
+'''
+class CNN(nn.Module):
+  def __init__(self, in_channel, num_classes):
+    super(CNN, self).__init__()
+    self.CNN_Layers = Sequential(
+        Conv2d(in_channel, 8, stride=1, kernel_size=3),
+        ReLU(inplace=False),
+        MaxPool2d(kernel_size=2, stride=2),
+        Conv2d(8, 32, stride=1, kernel_size=3),
+        ReLU(inplace=False),
+        MaxPool2d(kernel_size=3, stride=2),
+        Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+        ReLU(inplace=False),
+        Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+        ReLU(inplace=False),
+        MaxPool2d(kernel_size=2, stride=2),
+        Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+        ReLU(inplace=False),
+        Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+        ReLU(inplace=False),
+        MaxPool2d(kernel_size=2, stride=2)
+    )
+
+    self.FC_Layers = Sequential(
+        Dropout(inplace=False),
+        Linear(8192, 4096, bias=True),
+        ReLU(inplace=True),
+        Dropout(inplace=False),
+        Linear(4096, 1024, bias=True),
+        ReLU(inplace=True),
+        Dropout(inplace=False),
+        Linear(1024, num_classes, bias=True)        
+    )
+
+  def forward(self, x):
+    x = self.CNN_Layers(x)
+    x = x.view(x.size(0), -1)
+    x = self.FC_Layers(x)
+    return x
+'''
+
 '''
 class VGG19(nn.Module):
     def __init__(self, s_classes):
